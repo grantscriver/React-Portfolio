49be228cb68cde90273a84a7f43b8c810de8d7d9
Grant Scriver <grantscriver@gmail.com>
2020-11-28 21:09:36 -0600

    included basic footer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route exact path="/about" component={About} />
         <Route exact path="/project" component={Project} />
         <Route exact path="/contact" component={Contact} />
-
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Footer.css";
+
+function Footer() {
+
+    return (
+        <p class="footer"
+        >
+
+            Footer is here
+
+        </p>
+    )
+}
+
+export default Footer;
