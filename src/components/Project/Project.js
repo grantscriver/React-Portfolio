3f690058a35bc50ba180278bdfc57f4787607c0b
Grant Scriver <grantscriver@gmail.com>
2020-11-29 19:36:23 -0600

    improved formatting of portfolio page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,29 +1,36 @@
 import React from "react";
-// import "./style.css";
+import "./Project.css";
 
 function Project(props) {
     return (
-        <div className="">
-            <div className="img-container">
-                <img src={props.picture} />
-            </div>
-            <div className="content">
-                <h1 class="textColor">{props.name}</h1>
-                <p class="textColor">{props.description}</p>
-                <a class="textColor"
-                    type="button"
-                    className="btn btn-outline-light"
-                    href={props.gitHub}
-                >
-                    GitHub
+        <div className="container">
+            <div class="row mt-3">
+                <div class="co-12 col-md-4">
+                    <div className="img-container">
+                        <img src={props.picture}
+                            id="portfolioPhoto" />
+                    </div>
+                </div>
+                <div class="col-12 col-md-8">
+                    <div className="content">
+                        <h1 class="textColor">{props.name}</h1>
+                        <p class="textColor">{props.description}</p>
+                        <a class="textColor"
+                            type="button"
+                            className="btn btn-outline-light"
+                            href={props.gitHub}
+                        >
+                            GitHub
                 </a>
-                <a class="textColor"
-                    type="button"
-                    className="btn btn-outline-light"
-                    href={props.site}
-                >
-                    Live Site
+                        <a class="textColor"
+                            type="button"
+                            className="btn btn-outline-light"
+                            href={props.site}
+                        >
+                            Live Site
                 </a>
+                    </div>
+                </div>
             </div>
         </div>
     );
