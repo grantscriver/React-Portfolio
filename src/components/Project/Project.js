5962f5e7c1e350b09cbe52ea3ef554307c2bfd14
Grant Scriver <grantscriver@gmail.com>
2020-11-29 19:18:39 -0600

    populated project.JSON with project data

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -12,14 +12,14 @@ function Project(props) {
                 <p class="textColor">{props.description}</p>
                 <a class="textColor"
                     type="button"
-                    className="btn"
+                    className="btn btn-outline-light"
                     href={props.gitHub}
                 >
                     GitHub
                 </a>
                 <a class="textColor"
                     type="button"
-                    className="btn"
+                    className="btn btn-outline-light"
                     href={props.site}
                 >
                     Live Site
