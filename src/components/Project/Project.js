5bcf5992c1df50e01f53d626ee00915fb61223f0
Grant Scriver <grantscriver@gmail.com>
2021-02-06 17:48:32 -0600

    Updated structure of each project container

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,13 +4,13 @@ function Project(props) {
     return (
         <div className="container projects">
             <div className="row mt-3">
-                <div className="co-12 col-md-4">
+                <div className="co-12 col-md-5">
                     <div className="img-container pt-3">
                         <img src={process.env.PUBLIC_URL + props.picture}
                             id="portfolioPhoto" />
                     </div>
                 </div>
-                <div className="col-12 col-md-8">
+                <div className="col-12 col-md-7">
                     <div className="content">
                         <h3 className="textColor">{props.name}</h3>
                         <p className="textColor">{props.description}</p>
